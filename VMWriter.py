b0569d52ae8b4336219fb487c70b04ccdf19fea5
Kavya <gkavya10@gmail.com>
2016-11-08 13:04:57 +0530

    modify push and pop for field variables

--- VMWriter.py
+++ VMWriter.py
@@ -2,48 +2,54 @@
 
 class VMWriter(object):
 
-	def __init__(self, outfile):
-		self.vm = open(outfile, "w+")
+        def __init__(self, outfile):
+            self.vm = open(outfile, "w+")
 
-	def writePush(self, segment, index):
-		self.vm.write('push %s %d\n' %(segment, index)) 
+        def writePush(self, segment, index):
+            if segment == "field":
+                self.vm.write('push this %d\n' %(index)) 
 
-	def writePop(self, segment, index):
-		self.vm.write('pop %s %d\n' %(segment, index)) 
+            self.vm.write('push %s %d\n' %(segment, index)) 
 
-	def writeArithmetic(self, command):
-		self.vm.write(command.lower()+'\n')
+        def writePop(self, segment, index):
+            if segment == "field":
+                self.vm.write('pop this %d\n' %(index)) 
 
-	def writeLabel(self, label):
-		self.vm.write ('label %s\n' %(label)) 
+            self.vm.write('pop %s %d\n' %(segment, index)) 
 
-	def writeGoto(self, label):
-		self.vm.write('goto '+label+'\n')
+        def writeArithmetic(self, command):
+            self.vm.write(command.lower()+'\n')
 
-	def writeIf(self, label):
-		self.vm.write('if-goto '+label+'\n')
+        def writeLabel(self, label):
+            self.vm.write ('label %s\n' %(label)) 
 
-	def writeCall(self, name, nArgs):
-		self.vm.write('call %s %d\n' %(name, nArgs)) 
+        def writeGoto(self, label):
+            self.vm.write('goto '+label+'\n')
 
-	def writeFunction(self, name, nLocals):
-		self.vm.write('function %s %d\n' %(name, nLocals)) 
+        def writeIf(self, label):
+            self.vm.write('if-goto '+label+'\n')
 
-	def writeReturn(self):
-		self.vm.write('return\n')
+        def writeCall(self, name, nArgs):
+            self.vm.write('call %s %d\n' %(name, nArgs)) 
 
-	def close(self):
-		self.vm.close()
-'''
-v = VMWriter('vmcode.txt')
-v.writePush('local', 0)
-v.writePop('argument', 0)
-v.writeArithmetic('ADD')
-v.writeLabel('LOOP')
-v.writeGoto('LOOP')
-v.writeIf('END')
-v.writeCall('foo', 2)
-v.writeFunction('bar', 1)
-v.writeReturn()
-v.close()
-'''
+        def writeFunction(self, name, nLocals):
+            self.vm.write('function %s %d\n' %(name, nLocals)) 
+
+        def writeReturn(self):
+            self.vm.write('return\n')
+
+        def close(self):
+            self.vm.close()
+            '''
+            v = VMWriter('vmcode.txt')
+            v.writePush('local', 0)
+            v.writePop('argument', 0)
+            v.writeArithmetic('ADD')
+            v.writeLabel('LOOP')
+            v.writeGoto('LOOP')
+            v.writeIf('END')
+            v.writeCall('foo', 2)
+            v.writeFunction('bar', 1)
+            v.writeReturn()
+            v.close()
+            '''
