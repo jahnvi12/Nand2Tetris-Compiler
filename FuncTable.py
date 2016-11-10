e78d15d3e3a67bf6d554f0cf75f67cbbbfb25105
Rag1 <sraghavan02@gmail.com>
2016-11-10 10:40:49 +0530

    Function tables with basic defs

---
+++ FuncTable.py
@@ -0,0 +1,15 @@
+class FuncTable:
+    def __init__(self):
+        self.decfnlist={}
+        self.undecfnlist={}
+
+'''
+    def adddecfn(self,fname,fclass):
+        adds a declared function to list
+
+    def addundecfn(self,fname,fclass):
+        adds an unseen function to list
+
+    def removeundecfn(self,fname,fclass):
+        removes the unseen function from second table after declaration is seen
+'''
